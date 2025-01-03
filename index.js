cb2cdec413a62c5aa7eb13336182f4efcd064ba9
Fitriawan-Arya-N <nugrahaaryaa17@gmail.com>
2025-01-03 20:19:12 +0700

    update: melakukan update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! This is my Node.js app on Google Cloud Platform!');
+    res.send('Hello, World! This is my deployed Node.js app on Google Cloud Platform!');
 });
 
 app.listen(PORT, () => {
